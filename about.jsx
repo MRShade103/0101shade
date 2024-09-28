1bf4c8982756e461b4199433e3647ee96b7676ec
MRShade103 <davidwald0101@gmail.com>
2024-09-28 11:23:46 -0400

    About.jsx commit 2
    Add imports and comments

--- about.jsx
+++ about.jsx
@@ -1,3 +1,10 @@
+import Flick from './../public/Flick.png';
+import React from 'react';
+// File: about.jsx
+// Student: David Wald
+// StudentID: 301273421
+// Date: 9/27/2024
+
 export default function About() {
        
      return (
