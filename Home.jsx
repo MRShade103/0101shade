418696b601e826b27fe9efd720a486c57818048b
MRShade103 <davidwald0101@gmail.com>
2024-09-28 11:38:09 -0400

    Home.jsx commit 4
    Added comments

---
+++ Home.jsx
@@ -0,0 +1,11 @@
+// File: Home.jsx
+// Student: David Wald
+// StudentID: 301273421
+// Date: 9/27/2024
+
+export default function Home() {
+     return <p>     Hello My Fellow Ladies & Gentlemen! Look For creativity & adventure as it is everywhere including the smallest 
+           detail so that inspiration can to you even if it is not much!
+     </p>
+     }
+    
