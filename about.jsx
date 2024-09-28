95c76246b25f11c4f666f1cf29e13ffa08c3c101
MRShade103 <davidwald0101@gmail.com>
2024-09-28 11:19:44 -0400

    About commit 1
    beginning

---
+++ about.jsx
@@ -0,0 +1,14 @@
+export default function About() {
+       
+     return (
+      
+       <>
+            <p>About David</p>
+              
+             
+    
+    
+                   </>
+       
+     );
+       }
